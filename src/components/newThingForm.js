b8751f6671d3dc8492cfcb06dbfcd60e3c89eda3
TrianePeart <TrianePeart@gmail.com>
2023-02-11 11:40:23 -0500

    New Form Created - Submit not functioning

--- src/components/newThingForm.js
+++ src/components/newThingForm.js
@@ -1,3 +1,92 @@
-export default function NewForm(){
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'react-axios'
+const API = process.env.REACT_APP_API
+   
 
+export default function NewThingForm(){
+    let navigate = useNavigate();
+
+    const [postThing, setThing] = useState({
+        title: '', 
+        author: '', 
+        content: '', 
+        post_type: '',
+        update_at: '', 
+        created_at: '', 
+    }); 
+
+    const createNew = (makeThing) => {
+        axios
+        .post(`${API}/posts`, makeThing)
+        .then(
+            () => {
+                navigate(`/posts`);
+            }, 
+            (error) => console.error(error)
+        )
+        .catch((c) => console.warn('catch', c));
+    };
+
+    //This seems to be correct
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        createNew(postThing);
+    };
+
+    const handleTextChange = (event) => {
+        setThing({ ...postThing, [event.target.id]: event.target.value});
+    };
+
+    return(
+        <div className='NewThingz'>
+            <form onSubmit={handleSubmit}>
+                {/* Title Of Post*/} 
+                <label htmlFor='title'>Title:</label>
+                <input
+                id='title'
+                value={postThing.title}
+                type='text'
+                onChange={handleTextChange}
+                placeholder='Title Of Thing'
+                required
+                />
+                {/* Name Of Author */}
+                 <label htmlFor='author'>Author Name:</label>
+                <input
+                id='author'
+                value={postThing.author}
+                type='text'
+                onChange={handleTextChange}
+                placeholder="Written By"
+                require
+                />
+                {/* Category Of Post */}
+                <label htmlFor='post_type'>Category:</label>
+                <input
+                id='post_type'
+                value={postThing.post_type}
+                type='text'
+                onChange={handleTextChange}
+                placeholder="Random, Article, Poetry..."
+                require
+                />
+                {/* Content Of Post */}
+                 <label htmlFor='content'>Write Post</label>
+                <textarea
+                id='content'
+                value={postThing.content}
+                type='text'
+                onChange={handleTextChange}
+                require
+                />
+                 <br/>
+                <input type='submit'/>
+            </form>
+             <Link to={`/posts`}>
+                 <button>Cancel</button>
+             </Link>
+        </div>
+    );
 };
--- src/pages/NewThing.js
+++ src/pages/NewThing.js
@@ -1,7 +1,9 @@
+import NewForm from '../components/newThingForm'; 
 export default function NewPost(){
     return(
         <div>
             <h1>Create Page For New Tingz</h1>
+            <NewForm/>
         </div>
     )
 }
